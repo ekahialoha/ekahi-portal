d9ef53f9a6b4e12e000753a4e68a82a0a6d02617
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2019-12-06 18:44:00 -0800

    Implement basic devise JWT login/register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  def render_json(response)
+    if response.errors.empty?
+      render json: response
+    else
+      model_validation_error(response)
+    end
+  end
+
+  def model_validation_error(response)
+    render json: {
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: response.errors,
+          code: '100'
+        }
+      ]
+    }, status: :bad_request
+  end
 end
