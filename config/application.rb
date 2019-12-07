4450775389d900ad7c90b35c3b77ce39718b476f
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2019-12-06 17:15:30 -0800

    Add cookies and session to rails

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Portal
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add back functionality for cookies, will use for authentication
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
