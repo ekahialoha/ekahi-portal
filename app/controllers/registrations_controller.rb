aaddd174cc404c6b7bbef74a5a82bac51186a4f2
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2019-12-07 19:13:59 -0800

    Remove commneted out method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
     build_resource(sign_up_params)
 
     if resource.save
-      # render_json(resource)
       render_success_response(:created, resource)
     else
       render_exception_response(resource, :bad_request)
