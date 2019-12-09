74faedb4f3a64520ab21cbbbfb54686ae328b2ac
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2019-12-08 19:11:18 -0800

    Start of spec for Authentication Requests

---
+++ spec/requests/authentication_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpect.describe 'authentication', type: :request do
+
+  before(:each) do
+
+  end
+
+  describe 'POST /users/sign_in' do
+    before do
+
+    end
+
+    context 'when params is valid' do
+
+    end
+
+    content 'when params is missing or invalid' do
+
+    end
+  end
+
+  describe 'DELETE /users/sign_out' do
+
+  end
+end
