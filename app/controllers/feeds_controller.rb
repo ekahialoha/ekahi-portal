3c8d2ec800a1e7bd39ef075df89559d55b73887c
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2019-12-09 20:32:56 -0800

    Require authentication except for index/show

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,5 @@
 class FeedsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :current_feed, only: [:show, :update, :destroy]
 
   # GET /feeds
